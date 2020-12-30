ededc05e391acaee9e93c6bb3c02c4dccfe24e1c
Wellington dos Santos <wellington1998santoss@gmail.com>
2020-12-30 16:18:51 -0300

    Enviando responsividade

--- js/script.js
+++ js/script.js
@@ -10,5 +10,5 @@ function phoneMask (value) {
         .replace(/(\d{2})(\d)/, '($1) $2')
         .replace(/(\d{4})(\d)/, '$1-$2')
         .replace(/(\d{4})-(\d)(\d{4})/, '$1$2-$3')
-        .replace(/(-\d{4})\d+?$/, '$1')
+        .replace(/(-\d{4})\d+?$/, '$1');
 }
